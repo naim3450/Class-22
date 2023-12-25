b82b9da3b40f2b1ba5b35bdebfd91aab2cd4d2ba
Naim <143653983+naim3450@users.noreply.github.com>
2023-12-25 07:41:34 -0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+let reasult = 101
+
+if(reasult<0 || reasult>100){
+    console.log("Beta bal paknami koris keno")
+}
+else if(reasult>=80 && reasult<=100){
+    console.log("You Get A+")
+}
+else if(reasult >=70){
+    console.log("You Get A")
+}
+else if(reasult >=60){
+    console.log("You Get A-")
+}
+else if(reasult >=50){
+    console.log("You Get B")
+}
+else if(reasult >=40){
+    console.log("You Get C")
+}
+else if(reasult >=33){
+    console.log("You Get D")
+}
+else{
+    console.log("You Are Fail")
+}
